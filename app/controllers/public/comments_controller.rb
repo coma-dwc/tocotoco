3f11344989ce95fcd7bc9e0cd3ecad49eb67bc47
coma <tamagomapon@gmail.com>
2023-05-15 13:30:17 +0000

    [fix]コメント非同期通信、ページネーションレイアウト

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,6 +1,7 @@
 class Public::CommentsController < ApplicationController
 
   def create
+    @user = current_user
     @article = Article.find(params[:article_id])
     @comment = current_user.comments.new(comment_params)
     @comment.article_id = @article.id
@@ -8,6 +9,7 @@ class Public::CommentsController < ApplicationController
   end
 
   def destroy
+    @user = current_user
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
