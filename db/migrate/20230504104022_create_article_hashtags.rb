5d4f72379e9d23f717c9355c2ec9805ef669abef
coma <tamagomapon@gmail.com>
2023-05-20 10:18:01 +0000

    [fix]article_hasyutags.rb修正

--- db/migrate/20230504104022_create_article_hashtags.rb
+++ db/migrate/20230504104022_create_article_hashtags.rb
@@ -1,8 +1,8 @@
 class CreateArticleHashtags < ActiveRecord::Migration[6.1]
   def change
     create_table :article_hashtags do |t|
-      t.references :article, index: true, foreign_key: true
-      t.references :hashtag, index: true, foreign_key: true
+      t.references :article, index: true
+      t.references :hashtag, index: true
 
       t.timestamps
       t.index [:article_id, :hashtag_id], unique: true
